32d62cfab3f25d9b37e30b7ef42710c6916dc22b
AkshayGunshettiwar <gunshettiwarakshay066@gmail.com>
2021-06-27 10:30:50 +0530

    javascript programs

---
+++ JavascriptPrograms/scr.js
@@ -0,0 +1,9 @@
+function changeColor(){
+    document.body.style.backgroundColor="yellogreen";
+}
+
+let button1=document.getElementById('changeColor')
+
+button1.addEventListener('click',()=>{
+    document.body.style.backgroundColor="skyblue"
+});
